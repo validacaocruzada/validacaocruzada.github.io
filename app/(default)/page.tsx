7168fa461bb358897479b7b6bfebd40d61f7f101
joaopcoelho <joao@joaopcoelho.com>
2024-01-04 15:19:48 +0100

    remove newsletter banner

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,7 +5,6 @@ export const metadata = {
 
 import Hero from '@/components/hero'
 import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
 import Zigzag from '@/components/zigzag'
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
       <Hero />
       <Features />
       <Zigzag />
-      <Newsletter />
     </>
   )
 }
